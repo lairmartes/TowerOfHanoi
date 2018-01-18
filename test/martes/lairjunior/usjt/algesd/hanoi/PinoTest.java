45d8d48d894eb8d71431e09dc00969f40183dee4
lairmartes <lairmartes@gmail.com>
2018-01-18 11:39:20 -0200

    Including unit tests after successful running

---
+++ test/martes/lairjunior/usjt/algesd/hanoi/PinoTest.java
@@ -0,0 +1,22 @@
+package martes.lairjunior.usjt.algesd.hanoi;
+
+import org.junit.jupiter.api.Test;
+
+class PinoTest {
+
+    @Test
+    void adicionar() {
+    }
+
+    @Test
+    void removerDisco() {
+    }
+
+    @Test
+    void getDiscos() {
+    }
+
+    @Test
+    void iniciarPilhaDeDiscos() {
+    }
+}
